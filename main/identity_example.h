98bca70727afaee6a24701a7b58d05bbfe1b5deb
Aki Salminen <git@salminen.info>
2022-04-26 16:23:05 +0300

    Add Identity.h example

---
+++ main/identity_example.h
@@ -0,0 +1,13 @@
+// NOTE:
+//
+// Copy this file to identity.h for your editin pleasure
+//
+// The LoRaWAN frequency and the radio chip must be configured by running 'idf.py menuconfig'.
+// Go to Components / The Things Network, select the appropriate values and save.
+//
+// AppEUI (sometimes called JoinEUI)
+const char *appEui = "ffffffffffffffff";
+// DevEUI
+const char *devEui = "ffffffffffffffff";
+// AppKey
+const char *appKey = "ffffffffffffffffffffffffffffffff";
