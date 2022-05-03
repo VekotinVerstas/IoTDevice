d3f195b23d063450f35cd830d927bf92003bcce8
Aki Salminen <git@salminen.info>
2022-05-03 18:33:56 +0300

    Fix submodule issue

--- main/main.cpp
+++ main/main.cpp
@@ -50,6 +50,7 @@ void led_strip_hsv2rgb(uint32_t h, uint32_t s, uint32_t v, uint32_t *r, uint32_t
 static const char *TAG = "IoTdeviceLed";
 
 #define RMT_TX_CHANNEL RMT_CHANNEL_2
+#define EXAMPLE_CHASE_SPEED_MS (10)
 
 // Pins and other resources
 #define TTN_SPI_HOST      HSPI_HOST
@@ -67,8 +68,6 @@ static const char *TAG = "IoTdeviceLed";
 #define GPS_UART_RX_PIN         34
 #define GPS_UART_TX_PIN         12
 #define GPS_UART_RX_BUF_SIZE    (1024)
-
-// LedStrip pin 25 set in menuconfig
 
 static TheThingsNetwork ttn;
 const unsigned TX_INTERVAL = 60;
@@ -105,29 +104,34 @@ extern "C" void app_main(void)
 {
     void* loraMsg;
     esp_err_t err;
+    /*
+    //LED strip
+    uint32_t red = 0;
+    uint32_t green = 0;
+    uint32_t blue = 0;
+    uint16_t hue = 0;
+    uint16_t start_rgb = 0;
 
-    //Led strip chan 2, pin 25, 8 leds
-    ESP_LOGI(TAG, "LED start");
-    led_strip_t* strip = led_strip_init(RMT_TX_CHANNEL, CONFIG_EXAMPLE_RMT_TX_GPIO, CONFIG_EXAMPLE_STRIP_LED_NUMBER);
+    rmt_config_t config = RMT_DEFAULT_CONFIG_TX((gpio_num_t)CONFIG_EXAMPLE_RMT_TX_GPIO, RMT_TX_CHANNEL);
+    // set counter clock to 40MHz
+    config.clk_div = 2;
+
+    ESP_ERROR_CHECK(rmt_config(&config));
+    ESP_ERROR_CHECK(rmt_driver_install(config.channel, 0, 0));
+
+    // install ws2812 driver
+    led_strip_config_t strip_config = LED_STRIP_DEFAULT_CONFIG(CONFIG_EXAMPLE_STRIP_LED_NUMBER, (led_strip_dev_t)config.channel);
+    led_strip_t *strip = led_strip_new_rmt_ws2812(&strip_config);
     if (!strip) {
         ESP_LOGE(TAG, "install WS2812 driver failed");
     }
     // Clear LED strip (turn off all LEDs)
     ESP_ERROR_CHECK(strip->clear(strip, 100));
-
-    // Set some leds ( just to show that it works, needs plan what to do with leds
-    strip->set_pixel(strip, 0, 30, 0, 0);
-    strip->set_pixel(strip, 1, 0, 30, 0);
-    strip->set_pixel(strip, 2, 0, 0, 30);
-    strip->set_pixel(strip, 3, 30, 0, 0);
-    strip->set_pixel(strip, 4, 0, 30, 0);
-    strip->set_pixel(strip, 5, 0, 0, 30);
-    
-    // Flush RGB values to LEDs
-    ESP_ERROR_CHECK(strip->refresh(strip, 100));
-    ESP_LOGI(TAG, "LED done");
+    // Show simple rainbow chasing pattern
+    ESP_LOGI(TAG, "LED Rainbow Chase Start");
     //LEDSTRIP end
-
+    */
+    
     // Initialize the GPIO ISR handler service
     err = gpio_install_isr_service(ESP_INTR_FLAG_IRAM);
     ESP_ERROR_CHECK(err);
@@ -156,7 +160,10 @@ extern "C" void app_main(void)
     // Register callback for received messages
     ttn.onMessage(messageReceived);
 
+    //    ttn.setAdrEnabled(false);
+    //    ttn.setDataRate(kTTNDataRate_US915_SF7);
     //    ttn.setMaxTxPower(14);
+
     //    vTaskDelay(10000 / portTICK_PERIOD_MS);
     
     if (ttn.resumeAfterDeepSleep())
@@ -482,4 +489,50 @@ void read_and_parse_nmea(void* nmeaData)
     }
     uart_driver_delete(GPS_UART_NUM);
     free(buffer);
+}
+
+void led_strip_hsv2rgb(uint32_t h, uint32_t s, uint32_t v, uint32_t *r, uint32_t *g, uint32_t *b)
+{
+    h %= 360; // h -> [0,360]
+    uint32_t rgb_max = v * 2.55f;
+    uint32_t rgb_min = rgb_max * (100 - s) / 100.0f;
+
+    uint32_t i = h / 60;
+    uint32_t diff = h % 60;
+
+    // RGB adjustment amount by hue
+    uint32_t rgb_adj = (rgb_max - rgb_min) * diff / 60;
+
+    switch (i) {
+    case 0:
+        *r = rgb_max;
+        *g = rgb_min + rgb_adj;
+        *b = rgb_min;
+        break;
+    case 1:
+        *r = rgb_max - rgb_adj;
+        *g = rgb_max;
+        *b = rgb_min;
+        break;
+    case 2:
+        *r = rgb_min;
+        *g = rgb_max;
+        *b = rgb_min + rgb_adj;
+        break;
+    case 3:
+        *r = rgb_min;
+        *g = rgb_max - rgb_adj;
+        *b = rgb_max;
+        break;
+    case 4:
+        *r = rgb_min + rgb_adj;
+        *g = rgb_min;
+        *b = rgb_max;
+        break;
+    default:
+        *r = rgb_max;
+        *g = rgb_min;
+        *b = rgb_max - rgb_adj;
+        break;
+    }
 }
